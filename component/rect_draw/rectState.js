d3e499410f71fbe811b45c6aec6de078e68b7d1d
top1808 <top180802@gmail.com>
2021-10-03 13:28:35 +0700

    update rectState.js

--- component/rect_draw/rectState.js
+++ component/rect_draw/rectState.js
@@ -8,10 +8,10 @@ function limitDrag(deltaX, deltaY, rect, maxWidth, maxHeight) {
     if (height + deltaY > maxHeight)
         deltaY = maxHeight - height
     return {
-        x: deltaX < 0 ? 0 : deltaX,
-        y: deltaY < 0 ? 0 : deltaY,
-        width,
-        height,
+        x: Math.round(deltaX < 0 ? width / 2 : deltaX + width / 2),
+        y: Math.round(deltaY < 0 ? height / 2 : deltaY + height / 2),
+        width: Math.round(width),
+        height: Math.round(height),
     }
 }
 
@@ -32,29 +32,35 @@ function limitResize(x, y, rect, width, height, maxWidth, maxHeight) {
     if (y + height > maxHeight)
         height = maxHeight - y
     return {
-        x,
-        y,
-        width,
-        height
+        x: Math.round(x + width / 2),
+        y: Math.round(y + height / 2),
+        width: Math.round(width),
+        height:Math.round(height),
     }
 }
 
 const style = {
     border: "2px solid red",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "2.2rem",
+    color: 'rgba(230, 18, 18, 0.2)'
 };
 
 const RectDraw = props => {
-
+    
     const { rect, setRect, maxWidth, maxHeight } = props
+    console.log(rect)
 
     const handleResize = (e, direction, ref, delta, position, idx) => {
         let { width, height } = ref.style
         let { x, y } = position
 
-        x = Math.round(x)
-        y = Math.round(y)
         width = parseInt(width)
         height = parseInt(height)
+        x = Math.round(x)
+        y = Math.round(y)
         let temp = [...rect]
 
         temp[idx] = limitResize(x, y, temp[idx], width, height, maxWidth, maxHeight)
@@ -65,7 +71,6 @@ const RectDraw = props => {
         console.log(d, rect[idx])
         let temp = [...rect]
         temp[idx] = limitDrag(d.x, d.y, rect[idx], maxWidth, maxHeight)
-        console.log(temp[idx])
         setRect(temp)
     }
 
@@ -73,10 +78,11 @@ const RectDraw = props => {
         return <Rnd
             style={style}
             size={{ width: r.width, height: r.height }}
-            position={{ x: r.x, y: r.y }}
+            position={{ x: (r.x - r.width / 2), y: (r.y - r.height / 2)}}
             onDragStop={(e, d) => handleDrag(d, i)}
             onResizeStop={(e, direction, ref, delta, position) => handleResize(e, direction, ref, delta, position, i)}
         >
+            {i + 1}
         </Rnd>
     }))
 }
